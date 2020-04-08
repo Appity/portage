bca69356a8c7ecfc123e0bf928b1b6726372cd29
Scott Tadman <tadman@postageapp.com>
2020-04-07 21:17:52 -0400

    Reworking Portage::Queue to be more transparent

--- spec/unit/portage_queue_spec.rb
+++ spec/unit/portage_queue_spec.rb
@@ -1,27 +1,30 @@
 RSpec.describe Portage::Queue, type: :reactor, timeout: 1 do
   it 'can bridge between a Thread and a Reactor-driven Queue' do
-    queue_in, queue_out = Portage::Queue.pair
+    queue = Portage::Queue.new
 
-    expect(queue_in).to be_kind_of(Portage::Queue::Injector)
-    expect(queue_out).to be_kind_of(Async::Queue)
+    expect(queue).to be_kind_of(Portage::Queue)
 
+    signal = Async::Notification.new
+
+    list = [ ]
     count = 10
 
     Thread.new do
+      expect(Async::Task).to_not be_current
+
       count.times do |i|
-        queue_in << i
+        queue.async do
+          list << i
+        end
       end
 
-      queue_in << nil
+      queue.async do
+        signal.signal
+      end
     end
 
-    sleep(0.01)
+    signal.wait
 
-    dequeued = [ ]
-    queue_out.each do |i|
-      dequeued << i
-    end
-
-    expect(dequeued).to eq((0...count).to_a)
+    expect(list).to eq((0...count).to_a)
   end
 end
